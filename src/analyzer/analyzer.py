29b90af6acc5df9ee1960c1be11890e040635bfe
Yuxuan Jiang <jyuxuan@umich.edu>
2024-02-25 22:34:17 -0500

    add: trace collector & analyzer for 84911

---
+++ src/analyzer/analyzer.py
@@ -0,0 +1,164 @@
+import json
+
+
+class Event:
+    """
+    An event is a significant occurrence during the execution of a program.
+    e.g. API Invocations, Model Parameter Changes, etc.
+    """
+
+    def __init__(self, event: str):
+        self.event = event
+
+    def __repr__(self) -> str:
+        return self.event
+
+    def get_event(self):
+        return self.event
+
+    def set_event(self, event: str):
+        self.event = event
+
+
+class Trace:
+    """
+    A trace is a sequence of events that occurred during the execution of a program.
+    """
+
+    def __init__(self, events: list[Event]) -> None:
+        self.events = events
+
+    def analyze(self):
+        """
+        Current Status:
+        - This is specifically implemented for PyTorch-FORUM84911. The analysis tries to find events that always occur during function calls.
+        """
+        unique_processes = set()
+        unique_threads = set()
+        for event in self.events:
+            event_dict = json.loads(event.get_event())
+            unique_threads.add(event_dict["thread_id"])
+            unique_processes.add(event_dict["process_id"])
+
+        print(
+            f"Num of processes: {len(unique_processes)}, Process Ids: {unique_processes}"
+        )
+        print(f"Num of threads: {len(unique_threads)}, Thread Ids: {unique_threads}")
+
+        invariant_events_during_function_calls = {}
+        for pid in unique_processes:
+            for tid in unique_threads:
+                result = self.analyze_local(tid, pid)
+                for k, v in result.items():
+                    if k in invariant_events_during_function_calls:
+                        invariant_events_during_function_calls[k] = (
+                            invariant_events_during_function_calls[k].intersection(v)
+                        )
+                    else:
+                        invariant_events_during_function_calls[k] = v
+
+        return invariant_events_during_function_calls
+
+    def analyze_local(self, thread_id, process_id):
+        """
+        Analyze the trace for a specific thread in a specific process.
+        Assumes that within a thread, the events are ordered by their occurrence (True in Python).
+        """
+        function_call_pres = 0
+        function_call_posts = 0
+        state_variable_changes = 0
+        exception_events = 0
+
+        invariant_events_during_function_calls = {}
+        stack_current_function_calls = []
+        for i, event in enumerate(self.events):
+            # each event is a json string
+            event_dict = json.loads(event.get_event())
+            if (
+                event_dict["thread_id"] != thread_id
+                or event_dict["process_id"] != process_id
+            ):
+                continue
+
+            # if event_dict["function"] == "set_num_threads" or event_dict["function"] == "_get_tracing_state":
+            #     # TODO: This is a hack to ignore set_num_threads leading to assertion errors in the current implementation. We should handle this properly later.
+            #     continue
+
+            if event_dict["type"] == "function_call (pre)":
+                function_call_pres += 1
+
+                # add the function and its index to the stack
+                stack_current_function_calls.append(
+                    (event_dict["function"], event_dict["uuid"], i)
+                )
+
+            elif (
+                event_dict["type"] == "function_call (post)"
+                or event_dict["type"] == "function_call (post) (exception)"
+            ):
+                # TODO: handle the exception case separately. Currently, we are treating it as a normal function call post event in the analysis.
+                function_call_posts += 1
+
+                assert (
+                    len(stack_current_function_calls) > 0
+                ), "There should be a function call pre event before a function call post event"
+                assert (
+                    stack_current_function_calls[-1][0] == event_dict["function"]
+                ), f"The function call pre and post events should match, i: {i}, thread_id: {thread_id}, process_id: {process_id}, uuid: {event_dict['uuid']}, stack_call pre: {stack_current_function_calls[-1][0]}, curr post: {event_dict['function']}"
+
+                # pop the function from the stack
+                _, _, i_pre = stack_current_function_calls.pop()
+
+                # collect events between the function call pre and post events
+                events = self.events[i_pre : i + 1]
+
+                if event_dict["function"] in invariant_events_during_function_calls:
+                    invariant_events_during_function_calls[event_dict["function"]] = (
+                        invariant_events_during_function_calls[
+                            event_dict["function"]
+                        ].intersection(set(events))
+                    )
+                else:
+                    invariant_events_during_function_calls[event_dict["function"]] = (
+                        set(events)
+                    )
+
+            elif event_dict["type"] == "state_variable_change":
+                state_variable_changes += 1
+            elif event_dict["type"] == "exception":
+                exception_events += 1
+
+        print(
+            f"function_call_pres: {function_call_pres}, function_call_posts: {function_call_posts}, state_variable_changes: {state_variable_changes}, exception_events: {exception_events}"
+        )
+        return invariant_events_during_function_calls
+
+    def dump(self):
+        with open("trace.txt", "w") as f:
+            for event in self.events:
+                f.write(event.get_event() + "\n")
+        pass
+
+
+class TraceAnalyzer:
+    def __init__(self, traces: list[Trace]) -> None:
+        self.traces = traces
+        pass
+
+    def analyze(self):
+        pass
+
+
+if __name__ == "__main__":
+    # read the trace from a file
+    trace_lines = []
+    with open("log.txt", "r") as f:
+        trace_lines = [
+            Event(l.split(":trace:")[-1].strip())
+            for l in f.readlines()
+            if l.startswith("INFO:trace:") or l.startswith("ERROR:trace:")
+        ]
+
+    # create a trace object
+    trace = Trace(trace_lines)
+    trace.analyze()
--- src/instrumentor/tracer.py
+++ src/instrumentor/tracer.py
@@ -1,112 +1,259 @@
 import types
 import inspect
 import functools
+import torch
+import logging
+import os
+import sys
+import json
+import uuid
+import threading
+
+
+logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+logger = logging.getLogger(__name__)
+logger_trace = logging.getLogger("trace")
 
 
 def global_wrapper(original_function, *args, **kwargs):
-    print(f"Before calling {original_function.__name__}")
+    func_id = str(uuid.uuid4())
+    # Get the current thread object
+    current_thread = threading.current_thread()
+    # Get the thread ID
+    thread_id = current_thread.ident
+    process_id = os.getpid()
+
+    # logger_trace.info({'type': 'function_call (pre)', 'function': original_function.__name__, 'args': args, 'kwargs': kwargs})
+    logger_trace.info(
+        json.dumps(
+            {
+                "uuid": func_id,
+                "thread_id": thread_id,
+                "process_id": process_id,
+                "type": "function_call (pre)",
+                "function": original_function.__name__,
+            }
+        )
+    )
     try:
         result = original_function(*args, **kwargs)
     except Exception as e:
-        print(f"Exception in {original_function.__name__}: {e}")
-        print(f"args: {args}")
-        print(f"kwargs: {kwargs}")
+        logger_trace.error(
+            json.dumps(
+                {
+                    "uuid": func_id,
+                    "thread_id": thread_id,
+                    "process_id": process_id,
+                    "type": "function_call (post) (exception)",
+                    "function": original_function.__name__,
+                    "exception": str(e),
+                }
+            )
+        )
         raise e
-    print(f"After calling {original_function.__name__}")
+    # logger_trace.info({'type': 'function_call (post)', 'function': original_function.__name__, 'result': result})
+    logger_trace.info(
+        json.dumps(
+            {
+                "uuid": func_id,
+                "thread_id": thread_id,
+                "process_id": process_id,
+                "type": "function_call (post)",
+                "function": original_function.__name__,
+            }
+        )
+    )
     return result
+
 
 def wrapper(original_function):
     @functools.wraps(original_function)
     def wrapped(*args, **kwargs):
         return global_wrapper(original_function, *args, **kwargs)
+
     return wrapped
+
 
 instrumented_modules = set()
 skipped_modules = set()
 skipped_functions = set()
 
 # there are certain modules that we don't want to instrument (for example, download(), tqdm, etc.)
-modules_to_skip = [
-    'torch.fx'
-]
+modules_to_skip = ["torch.fx"]
+
 
 def instrument(pymodule: types.ModuleType, depth=0):
     if pymodule in instrumented_modules or pymodule in skipped_modules:
-        print(f"Depth:{depth}, Skipping module: ", pymodule.__name__)
+        logger.info(f"Depth: {depth}, Skipping module: {pymodule.__name__}")
         return 0
-    
-    print(f"Depth:{depth}, Instrumenting module: ", pymodule.__name__)
+
+    logger.info(f"Depth: {depth}, Instrumenting module: {pymodule.__name__}")
     instrumented_modules.add(pymodule)
-    
+
     count_wrapped = 0
     for attr_name in dir(pymodule):
         if not hasattr(pymodule, attr_name):
             # handle __abstractmethods__ attribute
-            print(f"Depth:{depth}, Skipping attribute as it does not exist: ", attr_name)
+            logger.info(
+                f"Depth: {depth}, Skipping attribute as it does not exist: {attr_name}"
+            )
             continue
 
-        attr = pymodule.__dict__.get(attr_name, None) # getattr(pymodule, attr_name)
+        attr = pymodule.__dict__.get(attr_name, None)  # getattr(pymodule, attr_name)
 
         if attr is None:
-            print(f"Depth:{depth}, Skipping attribute as it is None: ", attr_name)
+            logger.info(
+                f"Depth: {depth}, Skipping attribute as it is None: {attr_name}"
+            )
+            """
+            TODO: From my observation, this is happening for the attributes that are implemented in C extension, which usually include math ops and other low level operations for tensors
+            , such as tensor.add_.
+            We should support these operations as well. Reason is in PyTorch-FORUM84911.
+            """
             continue
-        
+
         # skip private attributes
-        if attr_name.startswith("_"):
-            print(f"Depth:{depth}, Skipping private attribute: ", attr_name)
+        if attr_name.startswith("__"):
+            logger.info(f"Depth: {depth}, Skipping magic functions: {attr_name}")
             if isinstance(attr, types.FunctionType):
                 skipped_functions.add(attr)
             elif isinstance(attr, types.ModuleType):
                 skipped_modules.add(attr)
             continue
+
+        """Current Issue with private attributes:
+
+        Background: 
         
-        if isinstance(attr, types.FunctionType) or isinstance(attr, types.BuiltinFunctionType):
+            There are actually no *private* attributes in Python. 
+            The single underscore prefix is a *convention* that is used to indicate 
+            that the attribute should not be accessed directly. 
+        
+            The double underscore function names such as `__init__` are actually 
+            'magic' functions in Python. 
+            These functions are super useful and are used to control the behavior of the class. 
+            To know more, refer to this link: https://rszalski.github.io/magicmethods/    
+            A lot of these magic functions, if instrumented, can be helpful. For example,
+            arguments to __init__ can be printed to understand how the class is initialized.
+            Also, incepting `__setattr__` can keep track of variable (e.g. weights and gradients) 
+            changes in the class.
+
+        Issue:
+            The problem is that if we are to instrument all the magic functions, the `__repr__` 
+            is leading to some troubles. 
+
+            ```
+            Before calling __repr__
+            Exception in __repr__: maximum recursion depth exceeded
+            Exception in extra_repr: maximum recursion depth exceeded while getting the repr of an object
+            Before calling __repr__
+            Before calling extra_repr
+            ```
+            This is because `__repr__` is called by `extra_repr` and `extra_repr` is called by `__repr__`.
+
+
+        Plan for now:
+            This feature is being delayed for now. The original motivation for tracking the magic functions is to 
+            capture invalid configs when initialting the classes. For example, if a optimizer is intialized with
+            no learnable parameters, it is a sign of a bug. 
+            However, we plan to delay this feature for now. The reasons are two fold:
+            1. In most ML pipelines, the class initialization code are only run once. This means if we want to 
+                infer invariants from these initializations, we need to combine trace from multiple pipelines. I think
+                it is better to just focus on the main training loop for now as these are executed multiple times and 
+                we can infer things from just one pipeline.
+            2. The second reason is these invalid initializations usually have symptoms during the training loop.
+                If the optimizer is not passed with learnable parameters, we will observe that no updates are being performed
+                to the model weights. This is a clear symptom that can be captured during the training loop.
+
+            So for now, we will skip the magic functions.
+        """
+
+        if isinstance(attr, types.FunctionType) or isinstance(
+            attr, types.BuiltinFunctionType
+        ):
             if attr in skipped_functions:
-                print(f"Depth: {depth}, Skipping function:", attr_name)
+                logger.info(f"Depth: {depth}, Skipping function: {attr_name}")
                 continue
 
-            print("Instrumenting function: ", attr_name)
+            logger.info(f"Instrumenting function: {attr_name}")
             wrapped = wrapper(attr)
             try:
                 setattr(pymodule, attr_name, wrapped)
             except Exception as e:
                 # handling immutable types and attrs that have no setters
-                print(f"Depth: {depth}, Skipping function {attr_name} due to error: {e}")
+                logger.info(
+                    f"Depth: {depth}, Skipping function {attr_name} due to error: {e}"
+                )
                 continue
             count_wrapped += 1
         elif isinstance(attr, types.ModuleType):
             if attr.__name__ in modules_to_skip:
-                print(f"Depth: {depth}, Skipping module due to modules_to_skip: ", attr_name)
+                logger.info(
+                    f"Depth: {depth}, Skipping module due to modules_to_skip: {attr_name}"
+                )
                 continue
 
             if attr in skipped_modules:
-                print(f"Depth: {depth}, Skipping module: ", attr_name)
+                logger.info(f"Depth: {depth}, Skipping module: {attr_name}")
                 continue
-            if not attr.__name__.startswith('torch'):
-                print(f"Depth: {depth}, Skipping module due to irrelevant name:", attr_name)
+            if not attr.__name__.startswith("torch"):
+                logger.info(
+                    f"Depth: {depth}, Skipping module due to irrelevant name:{attr_name}"
+                )
                 skipped_modules.add(attr)
                 continue
 
-            print(f"Depth: {depth}, Recursing into module: ", attr_name)
-            count_wrapped += instrument(attr, depth+1)
+            logger.info(f"Depth: {depth}, Recursing into module: {attr_name}")
+            count_wrapped += instrument(attr, depth + 1)
 
         elif inspect.isclass(attr):
-            print(f"Depth: {depth}, Recursing into class: ", attr_name)
-            if not attr.__module__.startswith('torch'):
-                print(f"Depth: {depth}, Skipping class {attr_name} due to irrelevant module:", attr.__module__)
+            logger.info(f"Depth: {depth}, Recursing into class: {attr_name}")
+            if not attr.__module__.startswith("torch"):
+                logger.info(
+                    f"Depth: {depth}, Skipping class {attr_name} due to irrelevant module: {attr.__module__}"
+                )
                 continue
-            count_wrapped += instrument(attr, depth+1)
+            count_wrapped += instrument(attr, depth + 1)
 
-    print(f"Depth: {depth}, Wrapped {count_wrapped} functions in module {pymodule.__name__}")
+    logger.info(
+        f"Depth: {depth}, Wrapped {count_wrapped} functions in module {pymodule.__name__}"
+    )
     return count_wrapped
 
-# instrument(torch)
 
-# print("calling torch.abs")
-# torch.abs(torch.tensor([-1., -2., 3.]))
-# print(torch.abs)
+class StateVarObserver:
+    """
+    Currently only suports torch models
+    """
 
-# linearlayer = torch.nn.Linear(10, 10)
-# print("calling linearlayer")
-# linearlayer(torch.randn(10))
-# linearlayer.forward(torch.randn(10))
+    def __init__(self, model):
+        # Get the current thread object
+        self.model = model
+        self.current_state = self._get_state_copy()
+
+    def _get_state_copy(self):
+        # Return a copy of the current state of the model
+        return {
+            name: param.clone().detach()
+            for name, param in self.model.named_parameters()
+        }
+
+    def has_changed(self):
+        # Check if there is any change in the model parameters
+        for name in self.current_state:
+            if not torch.equal(self.current_state[name], self.model.state_dict()[name]):
+                # logger_trace.info(f"State variable {name} has changed")
+                logger_trace.info(
+                    json.dumps(
+                        {
+                            "thread_id": threading.current_thread().ident,
+                            "process_id": os.getpid(),
+                            "type": "state_variable_change",
+                            "variable": name,
+                        }
+                    )
+                )
+                self.current_state = self._get_state_copy()
+                return True
+        self.current_state = self._get_state_copy()
+        return False
