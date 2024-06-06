8b77d5c41e90eafc0f8cfde222406de2e1be0c47
Yuxuan Jiang <jyuxuan@umich.edu>
2024-06-06 15:10:32 -0400

    add: typename try except block to handle exceptions thrown by cuBLASModule's custom getattr

--- mldaikon/utils.py
+++ mldaikon/utils.py
@@ -1,25 +1,36 @@
 import torch
 
 
+def safe_getattr(obj, attr, default=None):
+    """Safely get the attribute of an object.
+    try except is necessary as some objects (e.g. cuBLASModule in PyTorch) might have custom __getattr__
+    method that raises an exception when accessing certain attributes.
+    """
+    try:
+        return getattr(obj, attr, default)
+    except AssertionError:
+        return default
+
+
 def typename(o):
     if isinstance(o, torch.Tensor):
         return o.type()
-    module = ""
-    class_name = ""
-    if (
-        hasattr(o, "__module__")
-        and o.__module__ != "builtins"
-        and o.__module__ != "__builtin__"
-        and o.__module__ is not None
-    ):
-        module = o.__module__ + "."
-    if hasattr(o, "__qualname__") and isinstance(
-        o.__qualname__, str
+
+    module = safe_getattr(o, "__module__", "")
+    if module in ["buitins", "__builtin__", None]:
+        module = ""
+
+    class_name = safe_getattr(o, "__qualname__", "")
+    if not isinstance(
+        class_name, str
     ):  # the instance here is for the case when __qualname__ is _ClassNamespace
-        class_name = o.__qualname__
-    elif hasattr(o, "__name__"):
-        class_name = o.__name__
-    else:
-        class_name = o.__class__.__name__
+        class_name = ""
+
+    if not class_name:
+        class_name = safe_getattr(o, "__name__", "")
+    if not class_name:
+        class_name = safe_getattr(o, "__class__", type(o)).__name__
+
     assert isinstance(module, str) and isinstance(class_name, str)
-    return module + class_name
+
+    return f"{module}.{class_name}" if module else class_name
