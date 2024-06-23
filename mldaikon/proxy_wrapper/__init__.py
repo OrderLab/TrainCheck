e9aa3fce1aa8689711ee7de8251761ea7d3d6ebb
zimingzh <zimingzh@umich.edu>
2024-06-23 15:27:58 -0400

    fix: bring back the necessary import logic to make the observer utility inside torch_proxy.py executed before the instrumented code

---
+++ mldaikon/proxy_wrapper/__init__.py
@@ -0,0 +1,3 @@
+# This import is necessary to make the observer utility inside torch_proxy.py executed before the instrumented code. This would ensure the observer function is successfully registred before the instrumented code is executed.
+
+import mldaikon.proxy_wrapper.torch_proxy  # noqa
