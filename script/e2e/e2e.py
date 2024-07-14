fa2c184e97fc14da0b7eba7de4c6f2e6e789cbec
zimingzh <zimingzh@umich.edu>
2024-07-13 20:10:02 -0400

    e2e: support proxy_trace handling and infer_engine

--- script/e2e/e2e.py
+++ script/e2e/e2e.py
@@ -32,6 +32,16 @@ def run_python_script(
     return process.returncode
 
 
+def find_files(directory: str, prefix: str, suffix: str) -> list[str]:
+    # find all the file names inside the directory with the given suffix
+    files = []
+    for root, _, filenames in os.walk(directory):
+        for filename in filenames:
+            if filename.endswith(suffix) and filename.startswith(prefix):
+                files.append(os.path.join(root, filename))
+    return files
+
+
 def run_e2e(
     python_path: str,
     input_config: dict[str, str],
@@ -46,9 +56,11 @@ def run_e2e(
     modules_to_instrument: str = input_config["modules_to_instrument"]  # with -t flag
     proxy_module = input_config["proxy_module"]  # with --proxy_module flag
     proxy_log_dir = input_config["proxy_log_dir"]  # with --proxy_log_dir flag
+    api_log_dir = input_config["API_log_dir"]  # with --API_log_dir flag
     profiling = input_config["profiling"]  # with --profiling flag
+
     # run the script with the given arguments and environment variables
-    script_args: list[str] = [
+    trace_collector_script_args: list[str] = [
         "-m",
         "mldaikon.collect_trace",
         "-p",
@@ -59,6 +71,8 @@ def run_e2e(
         proxy_module,
         "--proxy_log_dir",
         proxy_log_dir,
+        "--API_log_dir",
+        api_log_dir,
         "--profiling",
         profiling,
     ]
@@ -66,11 +80,68 @@ def run_e2e(
     if os.path.exists(output_dir):
         os.system(f"rm -r {output_dir}")
     os.makedirs(output_dir)
+    os.makedirs(api_log_dir)
 
     # run the script with the given arguments and environment variables
     return_code = run_python_script(
-        python_path, script_args, input_env, output_dir
+        python_path, trace_collector_script_args, input_env, output_dir
     )  # ignore: type
+
+    if return_code != 0:
+        print(f"Error: the script returned with non-zero exit code: {return_code}")
+        print("E2E failed at collect_trace stage")
+        return return_code
+
+    # process the proxy_log.json file from output_dir
+    # example: python proxy_trace_process.py --input  ./output/PT84911/proxy_log.json
+    process_trace_script_args: list[str] = [
+        "proxy_trace_process.py",
+        "--input",
+        f"{output_dir}/proxy_log.json",
+    ]
+    return_code = run_python_script(
+        python_path, process_trace_script_args, {}, output_dir
+    )  # ignore: type
+
+    if return_code != 0:
+        print(f"Error: the script returned with non-zero exit code: {return_code}")
+        print("E2E failed at proxy_trace_process stage")
+        return return_code
+
+    ## Activate the Infer Engine
+    # example: python -m mldaikon.infer_engine -t <proxy_folder>/proxy_trace_processed_* <trace_folder>/<path_to_API_trace>
+    trace_folder = os.path.join(output_dir, "trace_log")
+    proxy_folder = os.path.join(output_dir, "processed_proxy_traces")
+
+    api_trace_folder = os.path.join(trace_folder, "API")
+
+    processed_proxy_files = find_files(
+        proxy_folder, prefix="proxy_trace_processed_", suffix=".json"
+    )
+    API_trace_files = find_files(api_trace_folder, prefix="_ml_daikon", suffix=".log")
+
+    import pdb
+
+    pdb.set_trace()
+
+    infer_engine_script_args: list[str] = [
+        "-m",
+        "mldaikon.infer_engine",
+        "-t",
+        " ".join(processed_proxy_files),
+        " ".join(API_trace_files),
+    ]
+
+    return_code = run_python_script(
+        python_path, infer_engine_script_args, {}, output_dir
+    )  # ignore: type
+
+    if return_code != 0:
+        print(f"Error: the script returned with non-zero exit code: {return_code}")
+        print("E2E failed at infer_engine stage")
+        return return_code
+    # the processed log file is now in output_dir/proxy_log_processed
+
     return return_code
 
 
@@ -89,6 +160,7 @@ if __name__ == "__main__":
     script_name = "PT84911"
     modules_to_instrument = ["megatron", "deepspeed", "torch"]
     output_dir = os.path.join(current_dir, f"../../output/{script_name}")
+    api_log_dir = os.path.join(output_dir, "trace_log")
     input_config: dict[str, str] = {
         "input_program": os.path.join(example_pipelines_dir, f"{script_name}.py"),
         "modules_to_instrument": " ".join(modules_to_instrument),
@@ -98,12 +170,10 @@ if __name__ == "__main__":
         # "funcs_of_inv_interest": None,
         "proxy_module": "model_transfer",
         "proxy_log_dir": output_dir,
+        "API_log_dir": api_log_dir,
         "profiling": "True",
     }
     input_env = {"PYTORCH_JIT": "0"}
-
-    if not os.path.exists(output_dir):
-        os.makedirs(output_dir)
     # run the e2e pipeline
     run_e2e(
         python_path=python_path,
