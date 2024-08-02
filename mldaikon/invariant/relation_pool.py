c84d7704bc170cd4f8762484ad8af65b177081e0
徐博宇 <xuboyu@umich.edu>
2024-08-01 22:50:29 -0400

    Create API call cover relation

--- mldaikon/invariant/relation_pool.py
+++ mldaikon/invariant/relation_pool.py
@@ -2,8 +2,9 @@ from typing import Type
 
 from mldaikon.invariant.consistency_relation import ConsistencyRelation
 from mldaikon.invariant.contain_relation import APIContainRelation
-
+from mldaikon.invariant.cover_relation import FunctionCoverRelation
 relation_pool: list[Type] = [
-    APIContainRelation,
-    ConsistencyRelation,
+    # APIContainRelation,
+    # ConsistencyRelation,
+    FunctionCoverRelation,
 ]
