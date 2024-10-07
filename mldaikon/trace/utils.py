b0ac59b9e25b27605fc1eb6ba570f45b03ef0b35
Yuxuan Jiang <jyuxuan@umich.edu>
2024-10-07 16:40:31 -0400

    add: skip flattening for args, kwargs and return values due to extremely diverse schema

--- mldaikon/trace/utils.py
+++ mldaikon/trace/utils.py
@@ -5,17 +5,21 @@ from collections.abc import MutableMapping
 from mldaikon.trace.types import MD_NONE
 
 
-def _flatten_dict_gen(d, parent_key, sep):
+def _flatten_dict_gen(d, parent_key, sep, skip_fields=None):
     for k, v in d.items():
         new_key = parent_key + sep + k if parent_key else k
-        if isinstance(v, MutableMapping):
+        if skip_fields and k in skip_fields:
+            yield k, v
+        elif isinstance(v, MutableMapping) and v != {}:
             yield from flatten_dict(v, new_key, sep=sep).items()
         else:
             yield new_key, v
 
 
-def flatten_dict(d: MutableMapping, parent_key: str = "", sep: str = "."):
-    return dict(_flatten_dict_gen(d, parent_key, sep))
+def flatten_dict(
+    d: MutableMapping, parent_key: str = "", sep: str = ".", skip_fields=None
+):
+    return dict(_flatten_dict_gen(d, parent_key, sep, skip_fields))
 
 
 def replace_none_with_md_none(obj):
@@ -36,7 +40,8 @@ def read_jsonlines_flattened_with_md_none(file_path: str):
             try:
                 docs.append(
                     flatten_dict(
-                        json.loads(line, object_hook=replace_none_with_md_none)
+                        json.loads(line, object_hook=replace_none_with_md_none),
+                        skip_fields=["args", "kwargs", "return_value"],
                     )
                 )
             except Exception as e:
