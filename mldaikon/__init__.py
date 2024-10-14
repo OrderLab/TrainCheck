bf93dc8304b685ffbfe72bcabbb5d9f2b96c0528
Yuxuan <lessoxx@gmail.com>
2024-10-13 22:41:26 -0400

    add: developer API to annotate training stages

--- mldaikon/__init__.py
+++ mldaikon/__init__.py
@@ -1,6 +1,8 @@
+from mldaikon.developer import instr_stage_annotation
+
 from .instrumentor.tracer import (  # Ziming: get rid of new_wrapper for now
     Instrumentor,
     get_all_subclasses,
 )
 
-__all__ = ["Instrumentor", "get_all_subclasses"]
+__all__ = ["Instrumentor", "get_all_subclasses", "instr_stage_annotation"]
