1be04592da9cdffcb47dd4bd52270a3ef79a37c2
Yuxuan Jiang <jyuxuan@umich.edu>
2024-11-11 15:49:52 -0500

    fix missing annotations.py

---
+++ mldaikon/developer/annotations.py
@@ -0,0 +1,45 @@
+from mldaikon.instrumentor import meta_vars
+import mldaikon.instrumentor.tracer as tracer
+
+def annotate_stage(stage_name: str):
+    """Annotate the current stage. This function should be invoked as the very first statement of the stage.
+    A stage is invalidated after a new stage annotation is encountered.
+
+    Allowed stage names: `init`, `training`, `evaluation`, `inference`, `testing`, `checkpointing`, `preprocessing`, `postprocessing`
+
+    Note that it is your responsibility to make sure this function is called on all threads that potentially can generate invariant candidates.
+    """
+
+    valid_stage_names = [
+        "init",
+        "training",
+        "evaluation",
+        "inference",
+        "testing",
+        "checkpointing",
+        "preprocessing",
+        "postprocessing",
+    ]
+
+    assert (
+        stage_name in valid_stage_names
+    ), f"Invalid stage name: {stage_name}, valid ones are {valid_stage_names}"
+
+    meta_vars["stage"] = stage_name
+
+def annotate_answer_start_token_ids(answer_start_token_ids: list[int], include_start_tokens: bool = False):
+    """Annotate the answer start token ids specifically for prompt generation tasks. If this is provided, ML-DAIKON will look for transformer.generate method calls
+    and use this information to only dump the response tokens.
+
+    Args:
+        answer_start_token_ids (list[int]): List of token ids that correspond to the start of the answer in the input sequence.
+        include_start_tokens (bool): Whether to include the start tokens in the generated prompt. Default is False.
+    
+    Note that if the start tokens are not found in the generated prompt, ML-DAIKON will assume that the response length is zero and dump an empty response.
+
+    If the answer_start_token_ids corresponds to the end of the input prompt, set include_start_tokens to False,
+    Otherwise the token_ids should correspond to the start of the answer in the input prompt, and include_start_tokens should be set to True.
+    """
+    tracer.GENERATE_START_TOKEN_IDS = answer_start_token_ids
+    tracer.GENERATE_START_TOKEN_IDS_INCLUDE_START_TOKENS = include_start_tokens
+
