5b4d2476262f29b916bb5257df277be9ae0cb3f6
Yuxuan Jiang <jyuxuan@umich.edu>
2024-11-22 19:33:35 +0000

    refactor: var periodic change generate hypo and collect examples

--- mldaikon/invariant/var_periodic_change_relation.py
+++ mldaikon/invariant/var_periodic_change_relation.py
@@ -42,7 +42,7 @@ def calculate_hypo_value(value) -> str:
 
 class VarPeriodicChangeRelation(Relation):
     @staticmethod
-    def infer(trace: Trace) -> tuple[list[Invariant], list[FailedHypothesis]]:
+    def generate_hypothesis(trace):
         """Infer Invariants for the VariableChangeRelation."""
         logger = logging.getLogger(__name__)
         ## 1. Pre-scanning: Collecting variable instances and their values from the trace
@@ -142,6 +142,58 @@ class VarPeriodicChangeRelation(Relation):
                         )
                     )
 
+        return list(all_hypothesis.values())
+
+    @staticmethod
+    def collect_examples(trace, hypothesis):
+        var_group_name = "var"
+        var_insts = trace.get_var_insts()
+
+        inv = hypothesis.invariant
+
+        # a simple check to see if the variable changing to a specific value occur for the matched variables
+        assert (
+            len(inv.params) == 1
+        ), "The number of parameters should be 1 for VarPeriodicChangeRelation."
+        var_param = inv.params[0]
+        assert isinstance(
+            var_param, (VarNameParam, VarTypeParam)
+        ), "The parameter should be VarTypeParam for VarPeriodicChangeRelation."
+
+        # for every variable instance, check whether it is interesting to the invariant
+        to_check_var_ids = []
+        for var_id in var_insts:
+            if var_param.check_var_id_match(var_id):
+                to_check_var_ids.append(var_id)
+
+        # for every to check variable instance, check the precondition match
+        # HACK: we use all the traces of the variable instances to check the precondition
+        for var_id in to_check_var_ids:
+            example_trace_records = [
+                attr_inst.traces[-1]
+                for attr_inst in var_insts[var_id][var_param.attr_name]
+            ]
+            example = Example({var_group_name: example_trace_records})
+
+            # check if the value is periodically set to the hypo value
+            hypo_value = var_param.const_value
+            occur_count = 0
+            for attr_inst in var_insts[var_id][var_param.attr_name]:
+                if calculate_hypo_value(attr_inst.value) == hypo_value:
+                    occur_count += 1
+
+            if not count_num_justification(occur_count):
+                # add negative example
+                hypothesis.negative_examples.add_example(example)
+            else:
+                # add positive example
+                hypothesis.positive_examples.add_example(example)
+
+    @staticmethod
+    def infer(trace: Trace) -> tuple[list[Invariant], list[FailedHypothesis]]:
+        logger = logging.getLogger(__name__)
+
+        all_hypothesis = VarPeriodicChangeRelation.generate_hypothesis(trace)
         # 4. find preconditions
         valid_invariants = []
         failed_hypothesis = []
